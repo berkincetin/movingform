d708fd43baab0207aa8593e4e05955c00a5c4e12
Berkin Cetin <bcetin14@gmail.com>
2020-06-16 14:23:06 -0400

    Started routing logicn for form

--- movingform/src/App.js
+++ movingform/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  route: 1
 }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+  onRouteChange = (route) => {
+  this.setState({route:route})
+  }
+  renderSwitch(movingStep) {
+    switch(movingStep) {
+      case 1:
+        return <h2>Form Step 1</h2>
+      case 2:
+          return <h1>Form Step 2</h1>
+      case 3:
+          return <h1>Form Step 3</h1>
+      case 4:
+          return <h1>Form Step 4</h1>
+    }
+  }
+  render () {
+    const { route } = this.state;
+    return (
+      <div className ="App">
+        <Header />
+        {      this.renderSwitch(route)  }
+        
+        
+      </div>
+    )
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ movingform/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        
+        <h1 className="mb-5">Multistage Moving Form Project</h1>
+
+    )
+}
+
+export default Header
