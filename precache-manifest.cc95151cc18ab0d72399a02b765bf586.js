1040a4949c76942fb50f07073c98168af1cf0afc
Berkin Cetin <bcetin14@gmail.com>
2020-06-17 16:54:22 -0400

    Updates

---
+++ precache-manifest.cc95151cc18ab0d72399a02b765bf586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd236d0ee1a8349adad4b379b3a6fa68",
+    "url": "/movingform/index.html"
+  },
+  {
+    "revision": "e2135d37450bc5f9dd46",
+    "url": "/movingform/static/css/main.14b526dd.chunk.css"
+  },
+  {
+    "revision": "3a2cfb6aa7d9bd3317a8",
+    "url": "/movingform/static/js/2.e53d1c6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movingform/static/js/2.e53d1c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2135d37450bc5f9dd46",
+    "url": "/movingform/static/js/main.60b57f05.chunk.js"
+  },
+  {
+    "revision": "527a8321880d01e9bbe7",
+    "url": "/movingform/static/js/runtime-main.1c9ec415.js"
+  }
+]);
